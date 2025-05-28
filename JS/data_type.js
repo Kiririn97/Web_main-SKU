40eb8bc6febf070e657914ac6c1ae06ae67ccb16
admin <dkatkfwkdmltlswh@tutanota.com>
2025-05-28 11:31:34 +0900

    13

---
+++ JS/data_type.js
@@ -0,0 +1,18 @@
+const sym1=Symbol('test'); //삼볼 함수로 값 생성
+let symbloVar1=sym1; //변수 초기화
+
+const airline=["비행기",320,"airbus",["V1",true]];
+//다양한 데이터 배열
+
+// 빈 갹채 생성
+const obj1={};
+
+// 속성을 추가하여 객체 생성
+const obj2={
+    name:"John Doe";
+    age 30,
+    isMale: true,
+};
+
+console.log(symbloVar1.toString());
+console.log(obj1,obj2,airline);
